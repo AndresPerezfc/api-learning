aacd0bbe78eaa5c4bcf3c0a128dd2864e01b485f
Andres Pereze <felipe3apc@gmail.com>
2024-03-25 05:36:56 -0500

    prueba API y conexión a Github

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(URL).then(res => res.json()).then(data => {
+
+async function reload(){
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
     const img = document.querySelector('img');
-    img.src =data[0].url
-})
+    img.src = data[0].url
+}
+
+reload()
